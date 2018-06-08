5a1a6b7dfa6423d4b2129ee2297ae85177f638f9
Brent Rainwater <brentvsmaximvs@gmail.com>
2018-06-08 16:01:48 -0400

    start JS
    Starting the javascript - read up on how to make a JS timer

--- memory-game.js
+++ memory-game.js
@@ -1,1 +1,13 @@
 ﻿// JavaScript for memory game
+
+//card list
+
+var listOfCards = ["", "", "", "", "", "",];
+	var moves = 0;
+	var foundMatch = 0;
+
+//open first card
+
+var startGame = false;
+
+//timer
